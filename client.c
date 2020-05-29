91b5f9bae5920f3df8656d440855e92a6001f2ea
Jairo <jairojr.p@hotmail.com>
2020-05-29 00:16:07 +0000

    Enviar dados ao servidor

--- client.c
+++ client.c
@@ -1,5 +1,6 @@
 #include <netinet/in.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
@@ -7,12 +8,13 @@ int main(int argc, char *argv[])
 {
     int socket_desc;
     struct sockaddr_in server;
+    char *message;
 
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 
     if(socket_desc == -1)
     {
-        printf("Não foi possivel ciar o socket\n");
+        printf("Não foi possivel ciar o socket!\n");
         return 1;
     }
 
@@ -22,11 +24,19 @@ int main(int argc, char *argv[])
 
     if (connect(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0)
     {
-        printf("Erro ao conectar\n");
+        printf("Erro ao conectar!\n");
         return 1;
     }
 
-    printf("Conectado\n");
-    
+    printf("Conectado.\n");
+
+    message = "GET / HTTP/1.1\r\n\r\n";
+    if (send(socket_desc, message, strlen(message), 0) < 0)
+    {
+        printf("Erro ao enviar!\n");
+        return 1;
+    }
+    printf("Dados enviados.\n");
+
     return 0;
 }
