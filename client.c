dc936b6b49dc2cc9c405c0435bd836190c674ab9
Jairo <jairojr.p@hotmail.com>
2020-05-29 00:07:02 +0000

    Conectar-se ao servidor

--- client.c
+++ client.c
@@ -1,15 +1,32 @@
+#include <netinet/in.h>
 #include <stdio.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 
 int main(int argc, char *argv[])
 {
-    int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    int socket_desc;
+    struct sockaddr_in server;
+
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 
     if(socket_desc == -1)
     {
-        printf("Não foi possivel ciar o socket");
+        printf("Não foi possivel ciar o socket\n");
         return 1;
     }
+
+    server.sin_addr.s_addr = inet_addr("172.217.28.132");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(80);
+
+    if (connect(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0)
+    {
+        printf("Erro ao conectar\n");
+        return 1;
+    }
+
+    printf("Conectado\n");
     
     return 0;
 }
