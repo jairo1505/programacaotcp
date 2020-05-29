c45c28fada0b29e05664ca5c48252d874bb64d8d
Jairo <jairojr.p@hotmail.com>
2020-05-29 01:06:25 +0000

    Servidor sempre ligado

--- server.c
+++ server.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
 {
     int socket_desc, c, new_socket;
     struct sockaddr_in server, client;
+    char *message, client_reply[2000];
 
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -35,17 +36,28 @@ int main(int argc, char *argv[])
     printf("Aguardando conexões...\n");
 
     c = sizeof(struct sockaddr_in);
-    new_socket = accept(socket_desc, (struct sockaddr *) &client, (socklen_t *) &c);
+    while((new_socket = accept(socket_desc, (struct sockaddr *) &client, (socklen_t *) &c)))
+    {
+        char *client_ip = inet_ntoa(client.sin_addr);
+        int client_port = ntohs(client.sin_port);
+
+        printf("Conexão aceita do cliente %s:%d\n", client_ip, client_port);
+
+        message = "Olá cliente!";
+        if (recv(new_socket, client_reply, 2000, 0) < 0 ){
+            printf("Falha no recv!\n");
+            return 1;
+        }
+        printf("Resposta Recebida.\n");
+        printf("%s\n", client_reply);
+
+        write(new_socket, message, strlen(message));
+    }
     if (new_socket < 0)
     {
         printf("Erro ao aceitar conexão!\n");
         return 1;
     }
-
-    char *client_ip = inet_ntoa(client.sin_addr);
-    int client_port = ntohs(client.sin_port);
-
-    printf("Conexão aceita do cliente %s:%d\n", client_ip, client_port);
 
     return 0;
 }
